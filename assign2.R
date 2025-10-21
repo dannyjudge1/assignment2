608006941e0522d9774b8f57f67c3e289caa0615
echu06 <ethanchu714@gmail.com>
2025-10-20 22:15:34 -0400

    Add regression model for body mass analysis
    Added a regression model to analyze body mass with categorical variables.

--- assign2.R
+++ assign2.R
@@ -11,3 +11,9 @@ penguins <- read_excel("penguins.xlsx")
 
 summary(penguins)
 skim(penguins)
+
+
+# adding a regression to the R file. Models a simple regression with three categorical variables and a fixed effects of time. 
+mod_1 <- aov(body_mass_g ~ island+species+ as.factor(year), data= penguins)
+summary(mod_1)
+plot(mod_1)
